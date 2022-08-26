f599a3e663ba186b54776268f7f16cdab30fb3bb
Ilya Erofeev <ilerofeev@yandex.ru>
2022-08-26 23:21:47 +0300

    firebase settings

--- src/context/firebase.ts
+++ src/context/firebase.ts
@@ -1,5 +1,15 @@
+import { FirebaseApp } from 'firebase/app'
 import { createContext } from 'react'
+import { Auth, SignInWithEmailAndPasswordType } from '../lib/firebase'
 
-const FirebaseContext = createContext<{ firebase: any; FieldValue: any } | null>(null)
+const FirebaseContext = createContext<{
+  firebase: FirebaseApp | null
+  getAuth: Auth | null
+  signInWithEmailAndPassword: SignInWithEmailAndPasswordType | null
+}>({
+  firebase: null,
+  getAuth: null,
+  signInWithEmailAndPassword: null,
+})
 
 export default FirebaseContext
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import FirebaseContext from './context/firebase'
-import { firebase, FieldValue } from './lib/firebase'
+import { firebase, getAuth, signInWithEmailAndPassword } from './lib/firebase'
+import './styles/app.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+    <FirebaseContext.Provider value={{ firebase, getAuth, signInWithEmailAndPassword }}>
       <App />
     </FirebaseContext.Provider>
   </React.StrictMode>
