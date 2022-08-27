c51f3f91ad1cc4f287d2c5ee75e218e7e1c8b5f0
Ilya Erofeev <dev.erofeev@gmail.com>
2022-08-27 17:32:01 +0300

    dashboard-page

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,22 @@ import { lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import * as ROUTES from './constants/routes'
 
-const Login = lazy(() => import('./pages/login'))
+const SignUp = lazy(() => import('./pages/sign-up'))
+const SignIn = lazy(() => import('./pages/sign-in'))
+const Dashboard = lazy(() => import('./pages/dashboard'))
+const NotFound = lazy(() => import('./pages/not-found'))
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<p>Loading...</p>} />
-      <Routes>
-        <Route path={ROUTES.LOGIN} element={<Login />}></Route>
-      </Routes>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path={ROUTES.SIGNUP} element={<SignUp />}></Route>
+          <Route path={ROUTES.SIGNIN} element={<SignIn />}></Route>
+          <Route path={ROUTES.DASHBOARD} element={<Dashboard />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   )
 }
-
-export default App
