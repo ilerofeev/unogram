e971e10ef130d068320e9a91cf9bb20ff2b89db3
Ilya Erofeev <dev.erofeev@gmail.com>
2022-08-30 16:42:09 +0300

    get profiles service method and suggestions component

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,5 +1,6 @@
 import { firestore } from '../lib/firebase'
-import { collection, getDocs, query, where } from 'firebase/firestore/lite'
+import { collection, getDocs, query, where, limit } from 'firebase/firestore/lite'
+import { User } from '../hooks/use-user'
 
 export async function doesUsernameExist(username: string) {
   const collectionRef = collection(firestore, 'users')
@@ -20,4 +21,19 @@ export async function getUserByUserId(userId: string) {
   }))
 
   return user
+}
+
+export async function getSuggestedProfiles(userId: string, following: string[]) {
+  const collectionRef = collection(firestore, 'users')
+  const q = query(collectionRef, where('userId', '!=', userId), limit(10))
+  const result = await getDocs(q)
+
+  const profiles = result.docs
+    .map((item) => ({
+      ...(item.data() as User),
+      docId: item.id,
+    }))
+    .filter((profile) => !following.includes(profile.userId))
+
+  return profiles
 }
