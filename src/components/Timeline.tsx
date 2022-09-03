86c190f48eb94584992e34dcc9b5f9ffa111eb6f
Ilya Erofeev <dev.erofeev@gmail.com>
2022-09-03 15:16:07 +0300

    timeline component

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,7 +1,19 @@
+import Skeleton from 'react-loading-skeleton'
+import usePhotos from '../hooks/use-photos'
+import Post from './post'
+
 export default function Timeline() {
-  return (
-    <div className="container col-span-2">
-      <p>im timeline</p>
-    </div>
-  )
+  const { photos } = usePhotos()
+
+  function renderPhotos() {
+    if (!photos) return <Skeleton count={4} width={640} height={500} className="mb-5" />
+
+    return photos.length > 0 ? (
+      photos.map((photo) => <Post key={photo.docId} content={photo} />)
+    ) : (
+      <p className="text-center text-2xl">Follow people to see photos</p>
+    )
+  }
+
+  return <div className="container col-span-2">{renderPhotos()}</div>
 }
