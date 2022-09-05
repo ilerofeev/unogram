7b6f4a761dadcf4d096345760cfcd36949a14db5
Ilya Erofeev <dev.erofeev@gmail.com>
2022-09-05 18:34:09 +0300

    profile component

---
+++ src/components/profile/index.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useReducer } from 'react'
+import Header from './header'
+import Photos from './photos'
+import { getUserPhotosByUserId } from '../../services/firebase'
+import { User } from '../../hooks/use-user'
+import { Photo } from '../../hooks/use-photos'
+
+type Reducer = {
+  profile: User
+  photosCollection: (Photo & { docId: string })[]
+  followerCount: number
+}
+
+export default function Profile({ user }: { user: User }) {
+  const reducer = (state: Reducer, newState: Reducer) => ({ ...state, ...newState })
+  const initialState = {
+    profile: {} as User,
+    photosCollection: [],
+    followerCount: 0,
+  }
+
+  const [{ profile, photosCollection, followerCount }, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    async function getProfileInfoAndPhotos() {
+      const photos = await getUserPhotosByUserId(user.userId)
+      dispatch({
+        profile: user,
+        photosCollection: photos,
+        followerCount: (user.followers && user.followers.length) || 0,
+      })
+    }
+    getProfileInfoAndPhotos()
+  }, [user.username])
+
+  return (
+    <>
+      <Header
+        photosCount={photosCollection ? photosCollection.length : 0}
+        profile={profile}
+        followerCount={followerCount}
+        setFollowerCount={dispatch}
+      />
+      <Photos photos={photosCollection} />
+    </>
+  )
+}
