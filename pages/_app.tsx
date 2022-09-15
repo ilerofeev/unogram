28ea5c97cdf08ddc30864bc4ea13b8d19183afd4
Ilya Erofeev <dev.erofeev@gmail.com>
2022-09-15 16:02:08 +0300

    files and folders structure update

---
+++ pages/_app.tsx
@@ -0,0 +1,26 @@
+import '../wdyr'
+// import '../styles/app.css'
+import '../styles/globals.css'
+import 'react-loading-skeleton/dist/skeleton.css'
+import FirebaseContext from '../context/firebase'
+import useAuthListener from '../hooks/use-auth-listener'
+import UserContext from '../context/user'
+import type { AppProps } from 'next/app'
+import { firestore } from '../lib/firebase'
+import { Suspense } from 'react'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const { user } = useAuthListener()
+
+  return (
+    <FirebaseContext.Provider value={{ firestore }}>
+      <UserContext.Provider value={user}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Component user={user} {...pageProps} />
+        </Suspense>
+      </UserContext.Provider>
+    </FirebaseContext.Provider>
+  )
+}
+
+export default MyApp
--- src/components/post/Image.tsx
+++
@@ -1,3 +0,0 @@
-export default function Image({ src, caption }: { src: string; caption: string }) {
-  return <img src={src} alt={caption} />
-}
