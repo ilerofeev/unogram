7e4aee33cc2823c92bf27ef9ba994160f0a85c41
Ilya Erofeev <dev.erofeev@gmail.com>
2022-09-17 14:11:57 +0300

    Post: upd styles

--- components/post/index.tsx
+++ components/post/index.tsx
@@ -2,7 +2,7 @@ import { useRef } from 'react'
 import { FollowedPhoto } from '../../hooks/use-photos'
 import Actions from './Actions'
 import Comments from './Comments'
-import Footer from './Footer'
+import Caption from './Caption'
 import PostHeader from './Post-header'
 import Image from 'next/image'
 
@@ -12,19 +12,22 @@ export default function Post({ content }: { content: FollowedPhoto }) {
   const handleFocus = () => commentInput.current?.focus()
 
   return (
-    <div
-      className="rounded col-span-4 border bg-white
-   border-gray-primary mb-12"
-    >
+    <div className="rounded col-span-4 border bg-white my-7">
       <PostHeader username={content.username} />
-      <Image src={content.imageSrc} alt={content.caption} width="675" height="845" />
+      <Image
+        src={content.imageSrc}
+        alt={content.caption}
+        width="768"
+        height="843"
+        className="w-full object-cover"
+      />
       <Actions
         docId={content.docId}
         totalLikes={content.likes.length}
         likedPhoto={content.userLikedPhoto}
         handleFocus={handleFocus}
       />
-      <Footer caption={content.caption} username={content.username} />
+      <Caption caption={content.caption} username={content.username} />
       <Comments
         docId={content.docId}
         comments={content.comments}
