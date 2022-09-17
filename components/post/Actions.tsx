3f5631df2e2b12f707c512a4aab5e104f3a3a4d0
Ilya Erofeev <dev.erofeev@gmail.com>
2022-09-17 14:10:53 +0300

    actions: update styles and likes functional

--- components/post/Actions.tsx
+++ components/post/Actions.tsx
@@ -1,7 +1,13 @@
 import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore/lite'
-import { useContext, useState } from 'react'
+import { KeyboardEvent, useContext, useState } from 'react'
 import UserContext from '../../context/user'
 import { firestore } from '../../lib/firebase'
+import {
+  BookmarkAltIcon,
+  ChatIcon,
+  HeartIcon as HeartIconFilled,
+  PaperAirplaneIcon,
+} from '@heroicons/react/outline'
 
 export default function Actions({
   docId,
@@ -28,58 +34,34 @@ export default function Actions({
       likes: toggleLiked ? arrayRemove(user?.uid) : arrayUnion(user?.uid),
     })
 
-    setLikes((likes) => (toggleLiked ? likes || 0 - 1 : likes || 0 + 1))
+    setLikes((likes) => (toggleLiked ? likes - 1 : likes + 1))
   }
 
   return (
     <>
-      <div className="flex justify-between p-4">
-        <div className="flex">
-          <svg
+      <div className="flex justify-between px-4 pt-3">
+        <div className="flex space-x-4">
+          <HeartIconFilled
+            className={`btn ${toggleLiked ? 'fill-red text-red-primary' : 'text-black-light'}`}
             onClick={handleToggleLiked}
-            onKeyDown={(event) => {
+            onKeyDown={(event: KeyboardEvent) => {
               if (event.key === 'Enter') {
                 handleToggleLiked()
               }
             }}
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            tabIndex={0}
-            className={`w-8 mr-4 select-none cursor-pointer focus:outline-none ${
-              toggleLiked ? 'fill-red text-red-primary' : 'text-black-light'
-            }`}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-            />
-          </svg>
-          <svg
+          />
+          <ChatIcon
+            className="btn"
             onClick={handleFocus}
-            onKeyDown={(event) => {
+            onKeyDown={(event: KeyboardEvent) => {
               if (event.key === 'Enter') {
                 handleFocus()
               }
             }}
-            className="w-8 text-black-light select-none cursor-pointer focus:outline-none"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            tabIndex={0}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-            />
-          </svg>
+          />
+          <PaperAirplaneIcon className="btn" />
         </div>
+        <BookmarkAltIcon className="btn" />
       </div>
       <div className="p-4 py-0">
         <p className="font-bold">{likes === 1 ? `${likes} like` : `${likes} likes`}</p>
